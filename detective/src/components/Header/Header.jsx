0e08079a8a5f9364527a2664d8062acb5e28d721
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2023-07-13 18:46:12 +0300

    init: add media

--- detective/src/components/Header/Header.jsx
+++ detective/src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import Burger from "../Nav/Burger.jsx";
 import Logo from "../../assets/images/logo.svg";
+import Phone from "../../assets/images/phone.svg";
+import Telegram from "../../assets/images/telegram.svg";
+import WhatsApp from "../../assets/images/whatsapp.svg";
 import style from "./header.module.scss";
 
 function Header() {
@@ -12,15 +15,21 @@ function Header() {
             <img src={Logo} alt="logo" className={style.logo} />
           </Link>
           <Burger />
-          {/* <h1 className="App__header__h1">English cards</h1>
-          <div className="App__header__buttons">
-            <Link to="/game">
-              <button className="App__header__game"></button>
-            </Link>
-            <Link to="/English-words-card">
-              <button className="App__header__home"></button>
-            </Link>
-          </div> */}
+          <div className={style.container}>
+            <div className={style.container_medium}>
+              <img src={Phone} alt="phone" className={style.phone} />
+              <div className={style.container_small}>
+                <p className={style.number}>+7 (925) 008-79-01</p>
+                <p className={style.timevisit}>Ежедневно с 8:00 до 22:00</p>
+              </div>
+            </div>
+            <a href="https://www.kinoafisha.info/releases/archive/">
+              <img src={Telegram} alt="Telegram" className={style.telegram} />
+            </a>
+            <a href="https://www.kinoafisha.info/releases/archive/">
+              <img src={WhatsApp} alt="WhatsApp" className={style.whatsapp} />
+            </a>
+          </div>
         </div>
       </header>
     </>
