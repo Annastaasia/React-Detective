59f1e999bfc085d8d9e83797c80d7bb68970331d
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2023-07-13 12:37:43 +0300

    init: create nav

---
+++ detective/src/assets/svg/Vectordown.jsx
@@ -0,0 +1,16 @@
+export default function Vectordown() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="13"
+      height="12"
+      viewBox="0 0 13 12"
+      fill="none"
+    >
+      <path
+        d="M9.14844 3.96973L6.67894 6.43973L4.20944 3.96973L3.14844 5.03073L6.67894 8.56073L10.2094 5.03073L9.14844 3.96973Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
---
+++ detective/src/components/Nav/Nav.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Vectordown from "../../assets/svg/Vectordown.jsx";
+import styled from "styled-components";
+import style from "./nav.module.scss";
+
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+
+  li {
+    padding: 18px 10px;
+  }
+
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color: #0d2538;
+    position: fixed;
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 300px;
+    padding-top: 3.5rem;
+    transition: transform 0.3s ease-in-out;
+
+    li {
+      color: #fff;
+    }
+  }
+`;
+
+const Nav = ({ open }) => {
+  return (
+    <nav>
+      <Ul open={open}>
+        <li>
+          Для частных лиц
+          <div className={style.vector}>
+            <Vectordown />
+          </div>
+        </li>
+        <li>
+          Для бизнеса
+          <div className={style.vector}>
+            <Vectordown />
+          </div>
+        </li>
+        <li>Цены</li>
+        <li>Блог</li>
+        <li>Отзывы</li>
+        <li>Контакты</li>
+      </Ul>
+    </nav>
+  );
+};
+
+export default Nav;
