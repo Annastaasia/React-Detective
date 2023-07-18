a8b45a6d9cb280543d3916dda2133d9a3557e92d
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2023-07-19 02:12:33 +0300

    feat: license mobil version

--- detective/src/pages/License/License.jsx
+++ detective/src/pages/License/License.jsx
@@ -1,23 +1,25 @@
 import style from "./license.module.scss";
 import LicenseImg from "../../assets/images/license.png";
-import LicenseImgSmall from "../../assets/images/license-small.png";
+// import LicenseImgSmall from "../../assets/images/license-small.png";
 
 export default function License() {
   return (
     <>
       <div className={style.container}>
         <img src={LicenseImg} alt="license" className={style.img} />
-        <img src={LicenseImgSmall} alt="license" className={style.imgsmall} />
+        {/* <img src={LicenseImgSmall} alt="license" className={style.imgsmall} /> */}
         <div className={style.blok}>
-        <h3 className={style.h3}>Лицензия</h3>
-        <p className={style.p}>
-          Наличие лицензии на профессиональную деятельность – одно из
-          преимуществ агентства «PERSHIN&PARTNERS». Наша компания имеет законное
-          право на оказание частных сыскных услуг физическим лицам и бизнесу.
-          <br /> <br />
-          Сотрудничество с лицензированными специалистами является для Заказчика
-          одной из дополнительных гарантий ответственности и профессионализма.
-        </p>
+          <h3 className={style.h3}>Лицензия</h3>
+          <p className={style.p}>
+            Наличие лицензии на профессиональную деятельность – одно из
+            преимуществ агентства «PERSHIN&PARTNERS». Наша компания имеет
+            законное право на оказание частных сыскных услуг физическим лицам и
+            бизнесу.
+            <br /> <br />
+            Сотрудничество с лицензированными специалистами является для
+            Заказчика одной из дополнительных гарантий ответственности и
+            профессионализма.
+          </p>
         </div>
       </div>
     </>
