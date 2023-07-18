a007d800c1d8dad0b8cc5af0f4f7624d5cedaeab
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2023-07-18 15:17:22 +0300

    feat: change form

--- detective/src/components/MainBG/MainBG.jsx
+++ detective/src/components/MainBG/MainBG.jsx
@@ -1,7 +1,47 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import InputMask from "react-input-mask";
 import Vectorright from "../../assets/svg/Vectorright.jsx";
 import style from "./mainbg.module.scss";
+import FormCheck from "../../assets/svg/FormCheck.jsx";
 
 function MainBG() {
+  // const [error, setError] = useState(false);
+
+  // const handleInputChange = (event) => {
+  //   const inputValue = event.target.value;
+  //   const hasError = inputValue.length === 0;
+  //   setError(hasError);
+  // };
+
+  // const [inputValue, setInputValue] = useState("");
+  // const [hasError, setHasError] = useState(false);
+
+  // const handleInputChange = (event) => {
+  //   let value = event.target.value;
+  //   setInputValue(value);
+
+  //   // Проверка наличия ошибки
+  //   if ((value = "")) {
+  //     setHasError(true);
+  //   } else {
+  //     setHasError(false);
+  //   }
+  // };
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  }; // your form submit function which will invoke after successful validation
+
+  console.log(watch("register")); // you can watch individual input by pass the name of the input
+
   return (
     <>
       <main>
@@ -9,20 +49,36 @@ function MainBG() {
           <div className={style.container}>
             <h1 className={style.h1}>perhin & partners</h1>
             <h2 className={style.h2}>детективное агентство в москве</h2>
-            <form className={style.form}>
-              <input
-                className={style.name}
-                type="text"
-                placeholder="Как к вам обращаться?"
-                name="name"
-              ></input>
 
-              <input
+            <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
+              <div className={style.wrapper}>
+                <input
+                  {...register("firstName", {
+                    required: true,
+                  })}
+                  className={style.name}
+                  type="text"
+                  placeholder="Как к вам обращаться?"
+                  name="name"
+                ></input>
+                {errors?.firstName?.type === "required" && (
+                  <div className={style.icon}>
+                    <FormCheck />
+                  </div>
+                )}
+                {/* {errors?.firstName?.type === "pattern" && (
+                  <p>Alphabetical characters only</p>
+                )} */}
+              </div>
+
+              <InputMask
                 className={style.number}
-                type="number"
+                type="text"
                 placeholder="Номер телефона"
                 name="number"
-              ></input>
+                mask="+7 (999) 999-99-99"
+                maskChar="_"
+              />
 
               <input
                 className={style.question}
@@ -39,6 +95,7 @@ function MainBG() {
               </button>
             </form>
           </div>
+
           <div className={style.button_mobile}>
             <button type="submit" className={style.button}>
               Оставить заявку
@@ -47,33 +104,6 @@ function MainBG() {
               </div>
             </button>
           </div>
-
-          {/* <form onSubmit={handleSubmit(onSubmit)}>
-            { <span>
-              <Input
-                {...register("phone")}
-                placeholder="+7 (999) 999-99-99"
-                inputComponent={PhoneMask}
-              />
-
-              {errors.phone && <p className="error">{errors.phone.message}</p>}
-            </span> }
-
-            <span>
-              <p>Email</p>
-              <Input {...register("email")} placeholder="example@mail.ru" />
-
-              {errors.email && (
-                <p className={style.error}>{errors.email.message}</p>
-              )}
-            </span>
-
-            <div className={style.start_button}>
-              <button type="submit" className={style.button}>
-                Lets go
-              </button>
-            </div>
-          </form> */}
         </div>
       </main>
     </>
