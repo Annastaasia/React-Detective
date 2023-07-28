2f1f82efc7cd351e2fb2323b4b1e81618b6c1bb6
lisa_ywl <yourworthlesslife@icloud.com>
2023-07-28 06:48:55 +0300

    modals in progress
    * desktop "how to contact us" is ready, but needs to be linked everywhere
    * added component Form
    * Form can be reused on main sceen, but should get more universal styles beforehand

--- detective/src/components/HowToReachUs/HowToReachUs.jsx
+++ detective/src/components/HowToReachUs/HowToReachUs.jsx
@@ -1,6 +1,8 @@
 import style from "./howtoreachus.module.scss"
 import { ReactComponent as Telegram } from "../../assets/images/footer-telegram.svg";
-// import { ReactComponent as WhatsApp } from "../../assets/images/footer-whatsapp.svg";
+import { ReactComponent as WhatsApp } from "../../assets/images/popup-whatsapp.svg";
+import { ReactComponent as Phone } from "../../assets/images/popup-phone.svg";
+import Form from "../Form/Form";
 
 export default function HowToReachUs() {
   return (
@@ -8,11 +10,38 @@ export default function HowToReachUs() {
       <h4 className={style.reach_us_title}>
         Как с нами связаться?
       </h4>
-      <p>Анонимно в мессенджерах или по телефону с руководителем</p>
-      <div className={style.reach_us_links}>
-        <Telegram />
-        {/* <WhatsApp /> */}
-      </div>
+      <section>
+        <p>Анонимно в мессенджерах или по телефону с руководителем</p>
+        <div className={style.reach_us_links}>
+          <a
+            className={style.reach_us_link}
+            href="https://t.me/pershin_detective"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Telegram />
+          </a>
+          <a
+            className={style.reach_us_link}
+            href="https://api.whatsapp.com/send?phone=79686868685&text=Доброго%20времени%20суток!"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <WhatsApp />
+          </a>
+          <a
+            className={style.reach_us_link}
+            href="tel:+7(925)008-79-01"
+          >
+            <Phone />
+            <p>+7 (925) 008-79-01</p>
+          </a>
+        </div>
+      </section>
+      <section>
+        <p>Или оставить заявку. Мы перезвоним вам в ближайшее время</p>
+        <Form isPopup={true} />
+      </section>
     </div>
   )
 }
--- detective/src/components/Popup/Popup.jsx
+++ detective/src/components/Popup/Popup.jsx
@@ -1,10 +1,11 @@
 import style from "./popup.module.scss"
+import { ReactComponent as Cross } from "../../assets/images/icon-white-cross.svg";
 
 export default function Popup(props) {
   return (props.trigger) ? (
-    <div className={style.popup} onClick={() => props.setTrigger(false)}>
+    <div className={style.popup}>
       <div className={style.popup_inner}>
-        <button className={style.close_btn} onClick={() => props.setTrigger(false)}>close</button>
+        <button className={style.close_btn} onClick={() => props.setTrigger(false)}><Cross /></button>
         {props.children}
       </div>
     </div>
