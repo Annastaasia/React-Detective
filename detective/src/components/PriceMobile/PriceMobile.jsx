1893962e477a072803ab401ed23422e107a4633d
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2023-07-28 06:16:26 +0300

    add: feat PriceMobile

---
+++ detective/src/components/PriceMobile/PriceMobile.jsx
@@ -0,0 +1,95 @@
+import style from "./pricemobile.module.scss";
+
+export default function PriceMobile() {
+  return (
+    <>
+      <div className={style.blok}>
+        <div className={style.blok_tablemobile}>
+          <p className={style.title}>Поиск родственников</p>
+          <div className={style.section_price}>
+            <div className={style.blok_price}>
+              <p className={style.text}>Минимальный аванс</p>
+              <p className={style.price}>От 50 000 руб</p>
+            </div>
+            <div className={style.blok_price}>
+              <p className={style.text}>Средний чек</p>
+              <p className={style.price}>10 000 руб</p>
+            </div>
+            <div className={style.blok_price}>
+              <p className={style.text}>Тип контракта</p>
+              <p className={style.price}>Депозитный</p>
+            </div>
+          </div>
+        </div>
+        <div className={style.blok_tablemobile}>
+          <p className={style.title}>Розыск имущества</p>
+          <div className={style.section_price}>
+            <div className={style.blok_price}>
+              <p className={style.text}>Минимальный аванс</p>
+              <p className={style.price}>От 50 000 руб</p>
+            </div>
+            <div className={style.blok_price}>
+              <p className={style.text}>Средний чек</p>
+              <p className={style.price}>10 000 руб</p>
+            </div>
+            <div className={style.blok_price}>
+              <p className={style.text}>Тип контракта</p>
+              <p className={style.price}>Депозитный</p>
+            </div>
+          </div>
+        </div>
+        <div className={style.blok_tablemobile}>
+          <p className={style.title}>Поиск человека по адресу</p>
+          <div className={style.section_price}>
+            <div className={style.blok_price}>
+              <p className={style.text}>Минимальный аванс</p>
+              <p className={style.price}>От 50 000 руб</p>
+            </div>
+            <div className={style.blok_price}>
+              <p className={style.text}>Средний чек</p>
+              <p className={style.price}>10 000 руб</p>
+            </div>
+            <div className={style.blok_price}>
+              <p className={style.text}>Тип контракта</p>
+              <p className={style.price}>Депозитный</p>
+            </div>
+          </div>
+        </div>
+        <div className={style.blok_tablemobile}>
+          <p className={style.title}>Розыск несовершеннолетних</p>
+          <div className={style.section_price}>
+            <div className={style.blok_price}>
+              <p className={style.text}>Минимальный аванс</p>
+              <p className={style.price}>От 50 000 руб</p>
+            </div>
+            <div className={style.blok_price}>
+              <p className={style.text}>Средний чек</p>
+              <p className={style.price}>10 000 руб</p>
+            </div>
+            <div className={style.blok_price}>
+              <p className={style.text}>Тип контракта</p>
+              <p className={style.price}>Депозитный</p>
+            </div>
+          </div>
+        </div>
+        <div className={style.blok_tablemobile}>
+          <p className={style.title}>Розыск имущества наследователя</p>
+          <div className={style.section_price}>
+            <div className={style.blok_price}>
+              <p className={style.text}>Минимальный аванс</p>
+              <p className={style.price}>От 50 000 руб</p>
+            </div>
+            <div className={style.blok_price}>
+              <p className={style.text}>Средний чек</p>
+              <p className={style.price}>10 000 руб</p>
+            </div>
+            <div className={style.blok_price}>
+              <p className={style.text}>Тип контракта</p>
+              <p className={style.price}>Депозитный</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
