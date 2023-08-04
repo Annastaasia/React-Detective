7af3613a8df34d6b8922e64da6c55f96e40efa9a
lisa_ywl <yourworthlesslife@icloud.com>
2023-08-04 11:21:06 +0300

    feat: link only on titles and button srvicies

--- detective/src/components/Card/Card.jsx
+++ detective/src/components/Card/Card.jsx
@@ -1,21 +1,26 @@
+import { Link } from "react-router-dom";
 import style from "./card.module.scss";
 
 export default function Card(props) {
   return (
-    <a href="/" className={`${style.card}`} key={`${props.id}`}>
+    <div className={`${style.card}`} key={`${props.id}`}>
       <img src={process.env.PUBLIC_URL + props.icon} className={`${style.card_icon}`} alt="" />
-      <p className={`${style.card_title}`}>{props.title}</p>
+      <Link to="/services_for_individuals/geolocation">
+        <p className={`${style.card_title}`}>{props.title}</p>
+      </Link>
       <p className={`${style.card_text}`}>{props.text}</p>
       <div className={`${style.card_price_container}`}>
         <p className={`${style.card_price}`}>{props.price}</p>
-        <div>
-          <span>Подробнее</span>
-          <img
-            src={process.env.PUBLIC_URL + "/images/biege_arrow_right.svg"}
-            alt="Нажмите, чтобы узнать подробнее"
-          />
-        </div>
+        <Link to="/services_for_individuals/geolocation">
+          <div>
+            <span>Подробнее</span>
+            <img
+              src={process.env.PUBLIC_URL + "/images/biege_arrow_right.svg"}
+              alt="Нажмите, чтобы узнать подробнее"
+            />
+          </div>
+        </Link>
       </div>
-    </a>
+    </div>
   );
 }
