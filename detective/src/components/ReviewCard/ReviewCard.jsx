10e5780435205652fb916ea4a9ea46db16e889dd
lisa_ywl <yourworthlesslife@icloud.com>
2023-08-04 10:37:53 +0300

    feat:link on name only  in reviews component and page

--- detective/src/components/ReviewCard/ReviewCard.jsx
+++ detective/src/components/ReviewCard/ReviewCard.jsx
@@ -24,36 +24,40 @@ export default function Review(props) {
   if (props.isLink === true) {
     return (
       <>
-        <Link to="/reviews">
-          <div className={`${style.review}`}>
-            <div className={`${style.review_stars}`}>
-              <span>{props.review}</span>
-              {stars}
-            </div>
-            <p className={`${style.review_name}`}>{props.name}</p>
-            <p className={`${style.review_date}`}>{props.date}</p>
-            <p className={`${style.review_text}`}>{props.text}</p>
-          </div>
-        </Link>
-      </>
-    );
-  } else {
-    return (
-      <a
-        target="_blank"
-        href="https://yandex.ru/web-maps/org/77630423623/reviews?reviews[publicId]=fnuuk197am45jz6ep2m0bg0jur&utm_source=review"
-        rel="noreferrer"
-      >
+
         <div className={`${style.review}`}>
           <div className={`${style.review_stars}`}>
             <span>{props.review}</span>
             {stars}
           </div>
-          <p className={`${style.review_name}`}>{props.name}</p>
+          <Link to="/reviews">
+            <p className={`${style.review_name}`}>{props.name}</p>
+          </Link>
           <p className={`${style.review_date}`}>{props.date}</p>
           <p className={`${style.review_text}`}>{props.text}</p>
         </div>
-      </a>
+
+      </>
+    );
+  } else {
+    return (
+
+      <div className={`${style.review}`}>
+        <div className={`${style.review_stars}`}>
+          <span>{props.review}</span>
+          {stars}
+        </div>
+        <a
+          target="_blank"
+          href="https://yandex.ru/web-maps/org/77630423623/reviews?reviews[publicId]=fnuuk197am45jz6ep2m0bg0jur&utm_source=review"
+          rel="noreferrer"
+        >
+          <p className={`${style.review_name}`}>{props.name}</p>
+        </a>
+        <p className={`${style.review_date}`}>{props.date}</p>
+        <p className={`${style.review_text}`}>{props.text}</p>
+      </div>
+
     );
   }
 }
